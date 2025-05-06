7ba97d9762ecee34a8e71d343c92a45bc705218c
mariamshamy <20812022200580@eng.zu.edu.eg>
2025-05-06 20:49:47 +0300

    Add files via upload

---
+++ MultiCycleMIPS/src/register_file/B_Register.vhd
@@ -0,0 +1,23 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity B_Register is
+    Port (
+        clk   : in  std_logic;
+        B_in  : in  std_logic_vector(31 downto 0);
+        B_out : out std_logic_vector(31 downto 0)
+    );
+end B_Register;
+
+architecture Behavioral of B_Register is
+    signal temp : std_logic_vector(31 downto 0);
+begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            temp <= B_in;
+        end if;
+    end process;
+
+    B_out <= temp;
+end Behavioral;
---
+++ MultiCycleMIPS/src/register_file/PC.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity pc is
+    port (
+        clk         : in  std_logic;
+        reset       : in  std_logic;  --clear
+        pc_write    : in  std_logic;  --load enable
+        pc_in       : in  std_logic_vector(31 downto 0);   -- value that will load to pc
+		
+        pc_out      : out std_logic_vector(31 downto 0)	   -- address that the pc will go to in memory  
+    );
+end pc;
+
+architecture Behavioral of pc is
+    signal temp1 : std_logic_vector(31 downto 0) := (others => '0');
+begin
+    process(clk, reset)
+    begin
+        if reset = '1' then			   -- Reset to 00000000 (active high reset)
+            temp1 <= (others => '0');
+        elsif rising_edge(clk) then
+            if pc_write = '1' then	
+                temp1 <= pc_in;       
+            end if;
+        end if;
+    end process;   
+    pc_out <= temp1; 
+	
+end architecture;
