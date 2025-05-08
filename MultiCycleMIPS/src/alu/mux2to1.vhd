c4ed39d718b00a34d3052f9672e52edb4761e01f
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-05-08 16:22:14 +0300

    Update mux2to1.vhd

--- MultiCycleMIPS/src/alu/mux2to1.vhd
+++ MultiCycleMIPS/src/alu/mux2to1.vhd
@@ -2,11 +2,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity mux2to1 is 
+entity mux2to1 is  
+	 generic (
+        N : integer := 32
+    );
+	
     port (
-	mux_in  : in  std_logic_vector(1 downto 0);	  
+	a, b   : in std_logic_vector(N-1 downto 0);
 	sel: in std_logic;
-       mux_out : out std_logic
+    mux_out : out  std_logic_vector(N-1 downto 0)
     );		
 end entity;
 
@@ -14,7 +18,7 @@ architecture sims of mux2to1 is
 begin
     with sel select
 	
-	mux_out <= mux_in(0) when '0',
-			   mux_in(1) when others;
+	mux_out <= a when '0',
+			   b when others;
 	
 end architecture;
