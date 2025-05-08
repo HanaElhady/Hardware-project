f540fa3852ba7b033e4f698dc7267ecb1624d042
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-05-08 16:23:14 +0300

    Update sign_extender.vhd

--- MultiCycleMIPS/src/alu/sign_extender.vhd
+++ MultiCycleMIPS/src/alu/sign_extender.vhd
@@ -4,12 +4,12 @@ use ieee.numeric_std.all;
 
 entity sign_extender is 
     port (
-        input16  : in  signed(15 downto 0);
-        output32 : out signed(31 downto 0)
+        input16  : in  std_logic_vector(15 downto 0);
+        output32 : out std_logic_vector(31 downto 0)
     );		
 end entity;
 
 architecture sims of sign_extender is
 begin 		
-    output32 <= resize(input16, 32);
+    output32 <= std_logic_vector(resize(signed(input16), 32));
 end architecture;
