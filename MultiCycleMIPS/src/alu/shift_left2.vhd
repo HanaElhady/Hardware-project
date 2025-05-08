39136705d842f019b5b961a1504a48f2008b9b8c
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-05-08 16:22:44 +0300

    Update shift_left2.vhd

--- MultiCycleMIPS/src/alu/shift_left2.vhd
+++ MultiCycleMIPS/src/alu/shift_left2.vhd
@@ -4,12 +4,12 @@ use ieee.numeric_std.all;
 
 entity shift_left2 is 
     port (
-        shift_in  : in  unsigned(31 downto 0);
-        shift_out : out unsigned(31 downto 0)
+        shift_in  : in  std_logic_vector(31 downto 0);
+        shift_out : out std_logic_vector(31 downto 0)
     );		
 end entity;
 
 architecture sims of shift_left2 is
 begin
-    shift_out <= shift_in SLL 2;
+    shift_out <= std_logic_vector(unsigned(shift_in) sll 2);
 end architecture;
