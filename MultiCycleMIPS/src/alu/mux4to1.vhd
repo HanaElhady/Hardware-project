9fbb1a92527730085c6c94228acbca1bc188643d
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-05-08 16:22:28 +0300

    Update mux4to1.vhd

--- MultiCycleMIPS/src/alu/mux4to1.vhd
+++ MultiCycleMIPS/src/alu/mux4to1.vhd
@@ -3,19 +3,22 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity mux4to1 is 
+	GENERIC (
+	N : INTEGER := 32
+	);
     port (
-        mux_in  : in  std_logic_vector(3 downto 0);	  
+        a,b,c,d : in  std_logic_vector(N-1 downto 0);
         sel     : in  std_logic_vector(1 downto 0);
-        mux_out : out std_logic
+        mux_out : out std_logic_vector(N-1 downto 0)
     );		
 end entity;
 
 architecture sims of mux4to1 is
 begin
     with sel select
-        mux_out <= mux_in(0) when "00",
-                   mux_in(1) when "01",
-                   mux_in(2) when "10",
-                   mux_in(3) when others;
+        mux_out <= a when "00",
+                   b when "01",
+                   c when "10",
+                   d when others;
 end architecture;
 
