a7213455fbd0d42c86d5c169b58aa551afada076
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-08-17 13:48:07 +0300

    Update memoryDataRegister.vhd

--- memoryDataRegister.vhd
+++ memoryDataRegister.vhd
@@ -3,25 +3,27 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity MemoryDataRegister is
-    port( 
-        CLK        : in  std_logic;
-        reset	   : in  std_logic;
-        input      : in  std_logic_vector(31 downto 0);
-        output     : out std_logic_vector(31 downto 0)
-    ); 
+  port( 
+    CLK    : in std_logic;
+    reset  : in std_logic;
+    input  : in std_logic_vector(31 downto 0);  
+    output : out std_logic_vector(31 downto 0) 
+  ); 
 end MemoryDataRegister;
 
 architecture Behavioral of MemoryDataRegister is
-    signal MemDataReg: std_logic_vector(31 downto 0) := (others => '0');
+  type mem_data_type is array (0 downto 0) of std_logic_vector(31 downto 0);
+  signal MemDataReg : mem_data_type := ((others => (others => '0')));
 begin
-    process(CLK)
-    begin
-        if reset = '1' then 
-            MemDataReg <= (others => '0');
-        elsif rising_edge(CLK) then
-            MemDataReg <= input;
-        end if;
-    end process;
+  process(CLK, reset)
+  begin
+    if reset = '1' then
+      MemDataReg(0) <= (others => '0');
+    elsif rising_edge(CLK) then
+      MemDataReg(0) <= input;
+    end if;
+  end process;
 
-    output <= MemDataReg;
+  output <= MemDataReg(0);
+
 end Behavioral;
