12ff2ade1f4fcf024019d2298a3f84039f307ada
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-08-17 13:53:03 +0300

    Update pc.vhd

--- pc.vhd
+++ pc.vhd
@@ -4,11 +4,12 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity pc is
     port (
-        clk       : in  std_logic;
-        reset     : in  std_logic;  -- Active-high reset
-        pc_write  : in  std_logic;  -- Load enable
-        pc_in     : in  std_logic_vector(31 downto 0);  -- Input value to load into PC
-        pc_out    : out std_logic_vector(31 downto 0)   -- Current PC output
+        clk         : in  std_logic;
+        reset       : in  std_logic;  --clear
+        pc_write    : in  std_logic;  --load enable
+        pc_in       : in  std_logic_vector(31 downto 0);   -- value that will load to pc
+		
+        pc_out      : out std_logic_vector(31 downto 0)	   -- address that the pc will go to in memory  
     );
 end pc;
 
@@ -17,15 +18,14 @@ architecture Behavioral of pc is
 begin
     process(clk, reset)
     begin
-        if reset = '1' then
+        if reset = '1' then			   -- Reset to 00000000 (active high reset)
             temp1 <= (others => '0');
         elsif rising_edge(clk) then
-            if pc_write = '1' then
-                temp1 <= pc_in;
+            if pc_write = '1' then	
+                temp1 <= pc_in;       
             end if;
         end if;
-    end process;
-
-    pc_out <= temp1;
-
+    end process;   
+    pc_out <= temp1; 
+	
 end architecture;
