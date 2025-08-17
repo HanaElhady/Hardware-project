9f004eb12914792219b2410200449141d86f7a97
HanaElhady <144938741+HanaElhady@users.noreply.github.com>
2025-08-17 13:59:32 +0300

    Update top.vhd

--- top.vhd
+++ top.vhd
@@ -3,62 +3,47 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity top is  
-	
     port (
-    clk, reset, load : in 	 std_logic;
-	pc_in 			 : inout std_logic_vector(31 downto 0);
-    pc_out       	 : out 	 std_logic_vector(31 downto 0);
-    instruction  	 : out   std_logic_vector(31 downto 0);
-    alu_result , alu_out   	 : out 	 std_logic_vector(31 downto 0);
-    mem_data_out 	 : out 	 std_logic_vector(31 downto 0);
-    reg_data1    	 : out 	 std_logic_vector(31 downto 0);
-    reg_data2    	 : out 	 std_logic_vector(31 downto 0)
+        clk   : in std_logic;
+        reset : in std_logic
     );	 
 end entity top;
 
 architecture sims of top is
 
-    signal A_mux_out, memData, C_mux_out,output32 : std_logic_vector(31 downto 0);
-    signal input_regA, output_regA, output_regB, input_regB , memory_data_register_output : std_logic_vector(31 downto 0);
-    signal rs, rd, ALUOut : std_logic_vector(31 downto 0);
-    signal b_shift_out, a_shift_out : std_logic_vector(31 downto 0);  
-    signal B_mux_out : std_logic_vector(4 downto 0);
-    signal ALUOP : std_logic_vector(1 downto 0);
-
-    signal zero, PCWrite, PCWriteCond, IorD, MemRead, MemWrite, IRWrite, MemtoReg, RegDst, RegWrite, ALUSrcA : std_logic;
-    signal ALUSrcB, PCSource : std_logic_vector(1 downto 0);
-    signal ALUControl : std_logic_vector(3 downto 0);
-
- 
-    signal jump_shift_input : std_logic_vector(31 downto 0);
-
-    -- Internal signals in top port	
-	signal instruction_internal , pc_out_internal , alu_result_internal :std_logic_vector(31 downto 0);
-    
+  constant PC_increment : std_logic_vector(31 downto 0) := "00000000000000000000000000000100";
+-- signals
+  signal PC_out, MuxToAddress, MemDataOut, MemoryDataRegOut, InstructionRegOut, MuxToWriteData,
+  ReadData1ToA, ReadData2ToB, RegAToMux, RegBOut, SignExtendOut, ShiftLeft1ToMux4, MuxToAlu,
+  Mux4ToAlu, AluResultOut, AluOutToMux, JumpAddress, MuxToPC : std_logic_vector(31 downto 0);
+  
+  signal ZeroCarry_TL, ALUSrcA_TL, RegWrite_TL, RegDst_TL, PCWriteCond_TL, PCWrite_TL, IorD_TL,
+  MemRead_TL, MemWrite_TL, MemToReg_TL, IRWrite_TL, ANDtoOR, ORtoPC : std_logic;
+  signal MuxToWriteRegister : std_logic_vector(4 downto 0);
+  signal ALUControltoALU : std_logic_vector(3 downto 0);
+  signal PCsource_TL, ALUSrcB_TL, ALUOp_TL : std_logic_vector(1 downto 0);
 
 begin
-	
-	 -- Assign internals to output port
-    instruction <= instruction_internal;
-	pc_out   	<= pc_out_internal;
-	alu_result  <= alu_result_internal;	
-	alu_out <= ALUOut;
-	
+
+  ANDtoOR <= ZeroCarry_TL and PCWriteCond_TL;
+  ORtoPC <= ANDtoOR or PCWrite_TL;
+  JumpAddress(31 downto 28) <= PC_out(31 downto 28); 
+  
     pc: entity work.pc
         port map (
             clk      => clk,
             reset    => reset,
-            pc_write => PCWrite,
-            pc_in    => pc_in,
-            pc_out   => pc_out_internal	 
+            pc_write => ORtoPC,
+            pc_in    => MuxToPC,
+            pc_out   => PC_out
         );
 
-    A_mux2to1: entity work.mux2to1
+    A_mux2to1: entity work.mux2to1	 
         port map (
-            a         => pc_out_internal, 
-            b         => ALUOut,
-            sel       => IorD,
-            mux_out   => A_mux_out
+            a         => PC_out, 
+            b         => AluOutToMux,
+            sel       => IorD_TL,
+            mux_out   => MuxToAddress
         );
 	
 	Memory : entity work.Memory 
@@ -66,170 +51,164 @@ begin
 	    	-- Inputs
 		    CLK       => clk,
 		    reset 	  => reset,
-		    address   => A_mux_out,	
-			MemRead   => MemRead,
-            MemWrite  => MemWrite,
-		    WriteData => output_regB,
-	   		MemData   => memData
+		    address   => MuxToAddress,	
+			MemRead   => MemRead_TL,
+            MemWrite  => MemWrite_TL,
+		    WriteData => RegBOut,
+	   		MemData   => MemDataOut
   );  
   InstructionRegister : entity work.InstructionRegister 
 	  port map(
 	    CLK       	    => clk,
 	    reset 	  	    => reset,   
-	    IRWrite   	    => IRWrite,
-	    in_instruction  => memData ,
-	    out_instruction => instruction_internal
+	    IRWrite   	    => IRWrite_TL,
+	    in_instruction  => MemDataOut ,
+	    out_instruction => InstructionRegOut
 	  );
   MemoryDataRegister : entity work.MemoryDataRegister
     port map ( 
         CLK       => clk,
 		reset 	  => reset,
-        input     => memData,
-        output    => memory_data_register_output
+        input     => MemDataOut,
+        output    => MemoryDataRegOut
     );
    
 
     B_mux2to1: entity work.mux2to1
-        generic map (N => 5)
+      generic map (N => 5)
         port map (
-            a       => instruction_internal(20 downto 16),  -- rt
-            b       => instruction_internal(15 downto 11),  -- rd
-            sel     => RegDst,
-            mux_out => B_mux_out
+            a       => InstructionRegOut(20 downto 16),  -- rt
+            b       => InstructionRegOut(15 downto 11),  -- rd
+            sel     => RegDst_TL,
+            mux_out => MuxToWriteRegister
         );
 
     C_mux2to1: entity work.mux2to1
         port map (
-            a       => ALUOut,
-            b       => memory_data_register_output,
-            sel     => MemtoReg,
-            mux_out => C_mux_out
+            a       => AluOutToMux,
+            b       => MemoryDataRegOut,
+            sel     => MemToReg_TL,
+            mux_out => MuxToWriteData
         );
 
     registerfile: entity work.registerfile
         port map (
             clk        => clk,
-            regWrite   => RegWrite,
-            readReg1   => instruction_internal(25 downto 21),
-            readReg2   => instruction_internal(20 downto 16),
-            writeReg   => B_mux_out,
-            writeData  => C_mux_out,
-            readData1  => input_regA,
-            readData2  => input_regB
+            regWrite   => RegWrite_TL,
+            readReg1   => InstructionRegOut(25 downto 21),
+            readReg2   => InstructionRegOut(20 downto 16),
+            writeReg   => MuxToWriteRegister,
+            writeData  => MuxToWriteData,
+            readData1  => ReadData1ToA,
+            readData2  => ReadData2ToB
         );
 
     A_Register: entity work.A_Register
         port map (
             clk   => clk,
-            A_in  => input_regA,
-            A_out => output_regA
+            A_in  => ReadData1ToA,
+            A_out => RegAToMux
         );
 
     B_Register: entity work.B_Register
         port map (
             clk   => clk,
-            B_in  => input_regB,
-            B_out => output_regB
+            B_in  => ReadData2ToB,
+            B_out => RegBOut
         );
 
     D_mux2to1: entity work.mux2to1
         port map (
-            a       => pc_out_internal,
-            b       => output_regA,
-            sel     => ALUSrcA,
-            mux_out => rs
+            a       => PC_out,
+            b       => RegAToMux,
+            sel     => ALUSrcA_TL,
+            mux_out => MuxToAlu
         );
 
     left_mux4to1: entity work.mux4to1
         port map (
-            a       => output_regB,
-            b       => X"00000004",
-            c       => output32 ,			
-            d       => b_shift_out,
-            sel     => ALUSrcB,
-            mux_out => rd
+            a       => RegBOut,
+            b       => PC_increment,
+            c       => SignExtendOut ,			
+            d       => ShiftLeft1ToMux4,
+            sel     => ALUSrcB_TL,
+            mux_out => Mux4ToAlu
         );
 
     Alu: entity work.Alu
         port map (
-            rs          => rs,
-            rd          => rd,
-            ALUControl  => ALUControl,
-            ALUResult   => alu_result_internal,
-            zero        => zero
+            rs          => MuxToAlu,
+            rd          => Mux4ToALU,
+            ALUControl  => ALUControltoALU,
+            ALUResult   => AluResultOut,
+            zero        => ZeroCarry_TL
         );
 
     ALUoutput: entity work.ALUout
         port map (
             clk     => clk,
             reset   => reset,
-            load    => load,
-            alu_in  => alu_result_internal,
-            alu_out => ALUOut
+            alu_in  => AluResultOut,
+            alu_out => AluOutToMux
         );
 
     right_mux4to1: entity work.mux4to1
         port map (
-            a       => alu_result_internal,
-            b       => ALUOut,
-            c       => a_shift_out,
+            a       => AluResultOut,
+            b       => AluOutToMux,
+            c       => JumpAddress,
             d       => (others => '0'),
-            sel     => PCSource,
-            mux_out => pc_in	
+            sel     => PCsource_TL,
+            mux_out => MuxToPC	
         );
 
     ALUControl_unit: entity work.ALUControl
         port map (
-            ALUOP      => ALUOP,
-            funct      => instruction_internal(5 downto 0),
-            ALUControl => ALUControl
+            ALUOP      => ALUOp_TL,
+            funct      => InstructionRegOut(5 downto 0),
+            ALUControl => ALUControltoALU
         );
 
     ControlFSM : entity work.ControlFSM  
         port map(
             clk          => clk,
             reset        => reset,
-            zero         => zero,
-            opcode       => instruction_internal(31 downto 26),
-            PCWrite      => PCWrite,
-            PCWriteCond  => PCWriteCond,
-            IorD         => IorD,
-            MemRead      => MemRead,
-            MemWrite     => MemWrite,
-            IRWrite      => IRWrite,
-            MemtoReg     => MemtoReg,
-            RegDst       => RegDst,
-            RegWrite     => RegWrite,
-            ALUSrcA      => ALUSrcA,
-            ALUSrcB      => ALUSrcB, 
-            ALUOp        => ALUOP,
-            PCSource     => PCSource
+			
+            zero         => ZeroCarry_TL, 
+			
+            opcode       => InstructionRegOut(31 downto 26),
+            
+			PCWrite      => PCWrite_TL,
+            PCWriteCond  => PCWriteCond_TL,
+            IorD         => IorD_TL,
+            MemRead      => MemRead_TL,
+            MemWrite     => MemWrite_TL,
+            IRWrite      => IRWrite_TL,
+            MemtoReg     => MemToReg_TL,
+            RegDst       => RegDst_TL,
+            RegWrite     => RegWrite_TL,
+            ALUSrcA      => ALUSrcA_TL,
+            ALUSrcB      => ALUSrcB_TL, 
+            ALUOp        => ALUOp_TL,
+            PCSource     => PCsource_TL
         );
 
     sign_extender: entity work.sign_extender
         port map (
-            input16  => instruction_internal(15 downto 0),
-            output32 => output32
+            input16  => InstructionRegOut(15 downto 0),
+            output32 => SignExtendOut
         );
-
-    --  Corrected jump input shift
-    jump_shift_input <= "000000" & instruction_internal(25 downto 0);
 
     A_shift_left2: entity work.shift_left2
         port map (
-            shift_in  => jump_shift_input,
-            shift_out => a_shift_out
+            shift_in  => SignExtendOut,
+            shift_out => ShiftLeft1ToMux4
         );
 
-    B_shift_left2: entity work.shift_left2
+    B_shift_left2: entity work.ShiftLeft
         port map (
-            shift_in  => output32,
-            shift_out => b_shift_out
+            shift_in  => InstructionRegOut(25 downto 0),
+            shift_out => JumpAddress(27 downto 0)
         );
-
-    -- Debug outputs
-    reg_data1    <= input_regA;
-    reg_data2    <= input_regB;
-    mem_data_out <= memData;
 
 end architecture sims;
